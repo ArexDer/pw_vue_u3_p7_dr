02ee26cf0909e19f85bb8a55830e8dffd0797f9f
Diego Rivas <darivas@uce.edu.ec>
2025-01-16 00:49:28 +0000

    inicio unidad3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+ <PaginaPokemon/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PaginaPokemon from './pages/PaginaPokemon.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PaginaPokemon
   }
 }
 </script>
---
+++ src/pages/PaginaPokemon.vue
@@ -0,0 +1,24 @@
+<template>
+   
+    <h2>Adivina el Pokemon de la IMAGEN</h2>
+    <PokemonImagen/>
+    <PokemonOpciones/>
+  
+</template>
+
+<script>
+import PokemonImagen from '../components/PokemonImagen.vue'
+import PokemonOpciones from '../components/PokemonOpciones.vue'
+export default {
+    name: 'PaginaPokemon',
+    components: {
+        PokemonImagen,
+        PokemonOpciones
+    }
+
+}
+</script>
+
+<style>
+
+</style>
